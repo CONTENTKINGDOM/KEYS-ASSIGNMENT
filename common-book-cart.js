61c876e32a22de3a94ed6f7a0076f3e8f4d6a9de
Yahya-Sirguroh <yahyasirguroh@gmail.com>
2023-10-24 15:54:32 +0530

    CK

---
+++ common-book-cart.js
@@ -0,0 +1,185 @@
+// common js
+// --
+// var perfEntries = performance.getEntriesByType("navigation");
+
+// if (perfEntries[0].type === "back_forward") {
+//     location.reload();
+// }
+// ------------
+
+const cart_Count_item = document.querySelector('.cart_Count_item');
+
+const increment = document.querySelectorAll('.increment');
+const decrement = document.querySelectorAll('.decrement');
+
+console.log(increment, 'increment');
+
+ArrayObj[0].myBooks.forEach((books) => {
+
+    increment.forEach((ele) => {
+        ele.addEventListener('click', (e) => {
+
+            let currentCard = e.target.closest(".book__card").dataset.card;
+            var quantity = e.target.closest(".book__card").querySelector('.quantity')
+            console.log(ele.id, 'ele.id');
+            console.log(quantity.value, 'testIncrementValur');
+
+            if (books.id == currentCard) {
+                quantity.value++;
+                books.orderQty = quantity.value;
+                let addTotalPrice = books.price;
+                books.totalPrice += addTotalPrice;
+                books.cartItem = 1;
+            }
+            localStorage.setItem('myBookData', JSON.stringify(ArrayObj))
+
+            CountPlusAllAmount()
+
+            if (books.cartItem > 0) {
+                additionItemCartLogo()
+            }
+        })
+    })
+})
+
+ArrayObj[0].myBooks.forEach((books) => {
+    decrement.forEach((ele) => {
+        ele.addEventListener('click', (e) => {
+
+            let currentCard = e.target.closest(".book__card").dataset.card;
+            var quantity = e.target.closest(".book__card").querySelector('.quantity')
+
+            // console.log(quantity.value, 'testDecrementValur');
+
+            if (books.id == currentCard) {
+                if (quantity.value > 0) {
+                    quantity.value--;
+                    books.orderQty = quantity.value;
+                    books.totalPrice = (books.totalPrice - books.price);
+                    if (books.orderQty == 0) {
+                        books.cartItem = 0;
+                    }
+                    if (books.cartItem == 0) {
+                        additionItemCartLogo()
+                    }
+                }
+            }
+            localStorage.setItem('myBookData', JSON.stringify(ArrayObj))
+
+            CountPlusAllAmount()
+
+            const sum = ArrayObj[0].myBooks.reduce((accumulator, object) => {
+                return accumulator + Number(object.orderQty);
+            }, 0);
+            console.log(sum, 'sum');
+
+
+        })
+    })
+})
+
+
+CountPlusAllAmount()
+
+// --
+// ArrayObj[0].myBooks.forEach((books) => {
+function additionItemCartLogo() {
+    if (cart_Count_item != null) {
+        const sumCartItem = ArrayObj[0].myBooks.reduce((accumulator, object) => {
+            return accumulator + Number(object.cartItem);
+        }, 0);
+        console.log(sumCartItem, 'sum');
+        cart_Count_item.innerHTML = sumCartItem;
+    }
+}
+additionItemCartLogo()
+// })
+
+const quantity = document.querySelectorAll('.quantity');
+
+function countQuantityFun() {
+    ArrayObj[0].myBooks.forEach((book) => {
+
+        quantity.forEach((ele) => {
+
+            if (book.id == ele.dataset.input) {
+                console.log("Hello");
+                ele.value = book.orderQty;
+            }
+
+        })
+    })
+}
+countQuantityFun();
+
+
+// -----
+var mouseVal = true;
+const cursor_move = document.body.addEventListener('mousemove', () => {
+    if (mouseVal == true) {
+        countQuantityFun();
+        console.log(1)
+        mouseVal = false;
+    }
+})
+const onTouch = document.body.addEventListener('touchstart', () => {
+    if (mouseVal == true) {
+        countQuantityFun();
+        console.log(1)
+        mouseVal = false;
+    }
+})
+// --- 
+
+
+
+
+// --
+
+function CountPlusAllAmount() {
+
+    const Tamount = document.querySelector('#Tamount');
+    const VAT = document.querySelector('#VAT');
+
+    let tamount_string = 0;
+
+    ArrayObj[0].myBooks.forEach((book) => {
+        tamount_string += book.totalPrice;
+
+    })
+    if (Tamount != null || VAT != null) {
+        Tamount.innerHTML = tamount_string;
+        VAT.innerHTML = tamount_string + 0;
+    }
+
+}
+
+
+// LAZY LOAD JS 
+
+const imgRef = document.querySelectorAll('.book__card img[data-src]')
+
+const lazyImg = (entries) => {
+
+    entries.forEach((entry) => {
+
+        console.log(entry, 'entry');
+        if (!entry.isIntersecting) return;
+
+        const targetImg = entry.target;
+        targetImg.src = targetImg.dataset.src;
+
+    })
+
+}
+
+const imageObserver = new IntersectionObserver(lazyImg, {
+    root: null,
+    threshold: 0,
+})
+
+imgRef.forEach((ele) => {
+    imageObserver.observe(ele);
+})
+
+
